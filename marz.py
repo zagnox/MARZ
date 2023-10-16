bdf62278784f4bf4fc2030e4a3b34533c0799f60
zagnox <zagnoxxx@proton.me>
2023-10-16 20:27:49 +0200

    added network manager restart function

--- marz.py
+++ marz.py
@@ -7,7 +7,10 @@
 # once, you don't have to manually change the MAC
 # using tools such as macchanger.
 
-file_path = '/etc/NetworkManager/conf.d/00-macrandomize.conf'
+import subprocess
+
+
+file_path = '/home/zagnox/Desktop/test.txt'
 file_content = """[device]
 wifi.scan-rand-mac-address=yes
 
@@ -16,11 +19,25 @@ wifi.cloned-mac-address=random
 ethernet.cloned-mac-address=stable
 connection.stable-id=${CONNECTION}/${BOOT}"""
 
-try:
-    with open(file_path, 'a') as file:
-        file.write(file_content)
-    print("File created successfully to ", file_path)
-except PermissionError:
-    print("Permission denied. Run as superuser or with appropriate permissions.")
-except Exception as e:
-    print("An error occurred: ", str(e))
+# creating the file at NetworkManager directory
+def create_file(path, content):
+    try:
+        with open(path, 'a') as file:
+            file.write(content)
+        print("File created successfully to ", path)
+    except PermissionError:
+        print("Permission denied. Run as superuser or with appropriate permissions.")
+    except Exception as e:
+        print("An error occurred: ", str(e))
+
+# restarting the NetworkManager
+def restart_network_manger():
+    try:
+        subprocess.run(['sudo', 'systemctl', 'restart', 'NetworkManager'], check=True)
+        print("NetworkManager restarted succesfully")
+    except subprocess.CalledProcessError as e:
+        print("Failed to restart NetworkManager: ", str(e))
+        print("You can restart NetworkManager manually on your terminal 'sudo systemctl restart NetworkManager'")
+
+create_file(file_path, file_content)
+restart_network_manger()
